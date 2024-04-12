493a2a62136ba4dffaed1d36ce5c6f2d1920ce21
Dmitriy Melnikov <d.melnikov@runetsoft.ru>
2024-04-12 11:38:59 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      modules: ["src", "node_modules"],
+    },
+  },
   devServer: {
     allowedHosts: "all",
   },
